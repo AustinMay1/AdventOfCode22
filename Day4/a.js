9ba772029a0c53649ffe94ee042524e5b23dca48
austin.may1 <austin.may@protonmail.com>
2022-12-07 12:08:14 -0500

    Day 4 Part 2

--- Day4/a.js
+++ Day4/a.js
@@ -5,15 +5,15 @@ const assignments = fs.readFileSync("./input.txt", "utf-8");
 // Check if either assignment pair is fully contained in the other
 // e.g. 1-3, 2-3: 2-3 is fully contained inside 1-3.
 
-// Break down each line into 4 seperate values
+// Break down each line into 4 seperate values, convert from string to Number
 // e.g. 28-88, 85-88 => [ 28, 88, 85, 88 ]
 const pairs = assignments
   .split("\n")
-  .map((pairs) => {
-    return pairs.replace("-", ",").trim();
+  .map((pair) => {
+    return pair.replace("-", ",").trim();
   })
   .map((range) => {
-    return range.replace("-", ",").split(",");
+    return range.replace("-", ",").split(",").map(Number);
   });
 
 console.log(pairs);
@@ -30,33 +30,13 @@ let overlapPairs = 0;
 // Determine if either set of numbers is fully contained in the other. Brute force method
 for (let i = 0; i < pairs.length; i++) {
   if (
-    (inRange(
-      Number(pairs[i][0]),
-      Number(pairs[i][2]),
-      Number(pairs[i][1]),
-      true
-    ) &&
-      inRange(
-        Number(pairs[i][0]),
-        Number(pairs[i][3]),
-        Number(pairs[i][1]),
-        true
-      )) ||
-    (inRange(
-      Number(pairs[i][2]),
-      Number(pairs[i][0]),
-      Number(pairs[i][3]),
-      true
-    ) &&
-      inRange(
-        Number(pairs[i][2]),
-        Number(pairs[i][1]),
-        Number(pairs[i][3]),
-        true
-      ))
+    (inRange(pairs[i][0], pairs[i][2], pairs[i][1], true) &&
+      inRange(pairs[i][0], pairs[i][3], pairs[i][1], true)) ||
+    (inRange(pairs[i][2], pairs[i][0], pairs[i][3], true) &&
+      inRange(pairs[i][2], pairs[i][1], pairs[i][3], true))
   ) {
     overlapPairs += 1;
   }
 }
 console.log(overlapPairs);
-//console.log(Number(pairs[0][0]));
+//console.log(pairs[0][0]));
---
+++ Day4/b.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const assignments = fs
+  .readFileSync("input.txt", "utf-8")
+  .replace(/\r/g, "")
+  .trim()
+  .split("\n");
+// Find pairs that overlap.
+// e.g. [5-7,7-9] = 5,6,7,8,9 || [2-8,3-7] = 2,3,4,5,6,7,8
+
+const pairs = assignments.map((pair) => {
+  const [first, second] = pair
+    .split(",")
+    .map((iv) => iv.split("-").map(Number))
+    .sort((a, b) => {
+      const one = a[1] - a[0];
+      const two = b[1] - b[0];
+      return two - one;
+    });
+
+    const overlap = first[1] >= second[0] && second[1] >= first[0];
+
+    return overlap ? 1 : 0;
+});
+
+[2,4] [5,6];
+
+console.log(pairs.reduce((a,b) => a + b, 0));
+
