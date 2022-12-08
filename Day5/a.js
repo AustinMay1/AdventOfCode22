e6b120e97dfa67d44e6c2373939baf91f16a2b61
austin.may1 <austin.may@protonmail.com>
2022-12-08 10:42:45 -0500

    Day 5 Part 1

--- Day5/a.js
+++ Day5/a.js
@@ -12,23 +12,41 @@ const crane = fs
       .map(Number)
       .filter((num) => {
         if (num != NaN) return num;
-      });
+      })
   });
 
-console.log(crane);
+// console.log(crane);
 
 // Move crates (letters) around iaw instructions
 // e.g. move 1 from 2 to 1 = stacks[2].pop() then stacks[1].push(valueFromPop);
 
 const stacks = {
-  1: ["z", "n"],
-  2: ["m", "c", "d"],
-  3: ["p"],
+  1: ['q','w','p','s','z','r','h','d'],
+  2: ['v','b','r','w','q','h','f'],
+  3: ['c','v','s','h'],
+  4: ['h','f','g'],
+  5: ['p','g','j','b','z'],
+  6: ['q','t','j','h','w','f','l'],
+  7: ['z','t','w','d','l','v','j','n'],
+  8: ['d','t','z','c','j','g','h','f'],
+  9: ['w','p','v','m','b','h']
 };
 
-// [1,2,1]
-let from = stacks[2].pop();
-stacks[1].push(from);
-console.log(stacks[1]);
+// go through each instruction and execute pop(amount) from, push(valueOfPop) to
+function move() {
+  for(const x of crane) {
+    for(let i = 0; i < x[0]; i++) {
+      const crate = stacks[x[1]].pop();
+      stacks[x[2]].push(crate);
+    }
+  }
+  console.log(stacks);
+}
+
+move();
+
+
+
+
 
 
