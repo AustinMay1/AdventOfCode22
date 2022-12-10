983f3cf5eb3f16b063ad265082c1c86118b38bb2
austin.may1 <austin.may@protonmail.com>
2022-12-09 21:18:24 -0500

    Day 6 Part 1

---
+++ Day6/a.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('input.txt', 'utf-8').trim();
+
+function isUnique(x) {
+    return new Set(x).size === x.length;
+}
+
+let subroutine = [];
+
+for(let i = 0; i < input.length; i++) {
+    subroutine.push(input[i]);
+    if(subroutine.length > 4) {
+        subroutine.shift();
+    }
+    if(subroutine.length === 4 && isUnique(subroutine)) { 
+        console.log(i + 1) 
+        break;
+    }
+}
